0e862b45e23e1238019b3bcc5ce1688ea88acf1b
anamilchert <ana.milchert@hotmail.com>
2024-05-13 15:10:50 -0300

    Criando customer

---
+++ Asaas/grails-app/domain/asaas/Customer.groovy
@@ -0,0 +1,17 @@
+package asaas
+
+import asaas.utils.BaseEntity
+
+class Customer extends BaseEntity {
+
+    String name
+    String email
+    String cpfCnpj
+    PersonType personType
+
+    static constraints = {
+        name blank: false
+        email email: true
+        cpfCnpj blank: false, maxSize: 16
+    }
+}
