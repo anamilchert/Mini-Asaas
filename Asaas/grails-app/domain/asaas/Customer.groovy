c2f36eb66d1e2ed3d7c4be6a03c1eeebd57a9b7e
anamilchert <ana.milchert@hotmail.com>
2024-05-14 10:23:41 -0300

    Adicionando namedQueries

--- Asaas/grails-app/domain/asaas/Customer.groovy
+++ Asaas/grails-app/domain/asaas/Customer.groovy
@@ -1,18 +1,30 @@
-package asaas
+    package asaas
 
-import asaas.utils.BaseEntity
+    import asaas.utils.BaseEntity
 
-class Customer extends BaseEntity {
+    class Customer extends BaseEntity {
 
-    String name
-    String email
-    String cpfCnpj
-    PersonType personType
+        String name
+        String email
+        String cpfCnpj
+        PersonType personType
 
-    static constraints = {
-        name blank: false
-        email blank: false, unique: true 
-        cpfCnpj size: 11..14, blank: false
-        personType blank: false
+        static constraints = {
+            name blank: false
+            email blank: false, unique: true 
+            cpfCnpj size: 11..14, blank: false
+            personType blank: false
+        }
+
+        static namedQueries = {
+            query { search ->
+            if (!Boolean.valueOf(search.includeDeleted)) {
+                eq('deleted', false)
+            }
+            if (search.containsKey('id')) {
+                eq('id', search.id)
+            }
+            }
+        }
+
     }
-}
