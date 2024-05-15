fff3c583380ce6fa3a78fdce87ce46c980d951d0
anamilchert <ana.milchert@hotmail.com>
2024-05-15 13:26:58 -0300

    Corrigindo pontuação

--- Asaas/grails-app/domain/asaas/Address.groovy
+++ Asaas/grails-app/domain/asaas/Address.groovy
@@ -24,7 +24,7 @@ class Address extends BaseEntity {
         neighborhood blank: false
         city blank: false
         state blank: false
-        CEP blank: false, size: 8:8
+        CEP blank: false, size: 8..8
     }
 
     static namedQueries = {
