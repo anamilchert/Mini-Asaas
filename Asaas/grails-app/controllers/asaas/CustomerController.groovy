0d1d535ac7170202e8b7cb0e2fb42e07741f0d78
anamilchert <ana.milchert@hotmail.com>
2024-05-16 10:10:14 -0300

    Criando service e controller

--- Asaas/grails-app/controllers/asaas/CustomerController.groovy
+++ Asaas/grails-app/controllers/asaas/CustomerController.groovy
@@ -1,1 +1,29 @@
 package asaas
+
+import grails.validation.ValidationException
+
+class CustomerController {
+
+    def customerService
+
+    def save() {
+        try {
+            Customer customer = customerService.save(params)
+            redirect(action: "show", id: customer.id)
+        } catch (ValidationException e) {
+            String errorsMessage = e.errors.allErrors.defaultMessage.join(", ")
+            flash.error = "Não foi possível salvar sua conta: $errorsMessage"
+            render(view: 'createCustomer', params: params)
+        }
+    }
+
+    def show(Long id) {
+        Customer customer = Customer.get(id)
+        if (customer) {
+            render(view: 'showCustomer', model: [customer: customer])
+        } else {
+            flash.error = "Conta não encontrada"
+            redirect(action: 'index')
+        }
+    }
+}
--- Asaas/grails-app/services/asaas/CustomerService.groovy
+++ Asaas/grails-app/services/asaas/CustomerService.groovy
@@ -1,1 +1,67 @@
 package asaas
+
+import grails.validation.ValidationException
+
+class CustomerService {
+
+    public Customer create(Map params) {
+
+        Customer customerValues = validateCustomerParams(params)
+
+        if (customerValues.hasErrors()) {
+            throw new ValidationException("Erro ao criar a conta", customerValues.errors)
+        }
+
+        Customer customer = new Customer(
+            name: params.name,
+            email: params.email,
+            cpfCnpj: params.cpfCnpj,
+            personType: params.personType
+        )
+
+        Address address = new Address(
+            street: params.address.street,
+            number: params.address.number,
+            neighborhood: params.address.neighborhood,
+            city: params.address.city,
+            state: params.address.state,
+            complement: params.address.complement,
+            CEP: params.address.CEP
+        )
+
+        customer.address = address
+
+        return saveCustomer(customer)
+    }
+
+    public Customer saveCustomer(Customer customer) {
+
+        return customer.save(flush: true, failOnError: true)
+    }
+
+
+    private Customer validateCustomerParams(Map params) {
+
+        Customer customer = new Customer()
+
+        if (!params.name) {
+            customer.errors.reject("name", null, "Nome é obrigatório")
+        }
+
+        if (!params.email) {
+            customer.errors.reject("email", null, "Email é obrigatório")
+        }
+
+        if (!params.cpfCnpj) {
+            customer.errors.reject("cpfCnpj", null, "CPF/CNPJ é obrigatório")
+        }
+
+        if (!params.address || !params.address.street || !params.address.number || 
+            !params.address.neighborhood || !params.address.city || !params.address.state || 
+            !params.address.CEP) {
+            customer.errors.reject("address", null, "Endereço incompleto")
+        }
+
+        return customer
+    }
+}
