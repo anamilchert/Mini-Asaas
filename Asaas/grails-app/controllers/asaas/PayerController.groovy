33481bc8442b99a49488fd663a0e574b6f375303
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-05-17 12:19:11 -0300

    Adaptando service e controller sem a necessidade de dto

--- Asaas/grails-app/controllers/asaas/PayerController.groovy
+++ Asaas/grails-app/controllers/asaas/PayerController.groovy
@@ -1,1 +1,38 @@
 package asaas
+
+import asaas.Payer
+import asaas.Customer
+import asaas.PayerService
+
+class PayerController {
+
+  def payerService
+  
+  def index(){
+    // Temporario, pois não temos ainda o forma de pegar um customer
+    try {
+      List<Customer> customers = Customer.list()
+      [customers: customers]
+    }catch (Exception e){
+      println e
+    }
+  }
+
+  def save(){
+    try{
+      Payer payer = payerService.save(params)
+      redirect(action:"show", id:payer.id)
+    }catch (Exception e){
+      println e
+      return "Error when creating payer"
+    }
+  }
+
+  def show(Long id){
+    Payer payer = Payer.get(id)
+    if(payer){
+      return [payer: payer]
+    }
+    render "Página não encontrada"
+  }
+}
--- Asaas/grails-app/services/asaas/PayerService.groovy
+++ Asaas/grails-app/services/asaas/PayerService.groovy
@@ -1,24 +1,79 @@
 package asaas
 
 import grails.gorm.transactions.Transactional
+import grails.validation.ValidationException
 import asaas.Customer
 import asaas.Address
 import asaas.Payer
-import asaas.AddressService
-import asaas.dto.CreatePayerFormDTO
-import asaas.dto.CreatePayerDTO
+import asaas.PersonType
 
 @Transactional
 class PayerService {
-  AddressService addressService
+  def save(Map params){
 
+    Payer payerValidation = validation(params)
 
-  def save(CreatePayerFormDTO data, Customer customer){
-    Address address = addressService.save(data.address)
-    Payer payer = CreatePayerDTO.createPayer(data.payer)
+    if(payerValidation.hasErrors()){
+      throw new ValidationException("Erro ao criar um pagador", payerValidation.errors)
+    }
+    
+    Address address = new Address(
+      street: params.street,
+      number: params.number,
+      neighborhood: params.neighborhood,
+      city: params.city,
+      state: params.state,
+      complement: params.complement,
+      CEP: params.CEP
+    )
+
+    Customer customer = Customer.load(params.customerId)
+
+    Payer payer = new Payer(
+      name: params.name,
+      email: params.email,
+      cpfCnpj: params.cpfCnpj,
+      phone: params.phone,
+      personType: PersonType.convert(params.personType)
+    )
+
     payer.address = address
     payer.customer = customer
-    payer.save(failOnError: true)
+    
+    payer.save(flush: true, failOnError: true)
+
     return payer
   }
+
+
+
+  private Payer validation(Map params){
+    Payer payer = new Payer()
+    
+    if(!params.name){
+      payer.errors.reject("name", null, "Nome é obrigatório")
+    }
+
+    if(!params.email){
+      payer.errors.reject("email", null, "Email é obrigatório")
+    }
+
+    if(!params.phone){
+      payer.errors.reject("phone", null, "Phone é obrigatório")
+    }
+
+    if(!params.cpfCnpj){
+      payer.errors.reject("cpfCnpj", null, "CPF/CNPJ é obrigatório")
+    }
+
+    if (!params.street || !params.number || 
+            !params.neighborhood || !params.city || !params.state || 
+            !params.CEP) 
+      {
+        payer.errors.reject("address", null, "Endereço incompleto")
+      }
+
+    return payer
+  }
+
 }
