3a545e80856314cc93147814b3971ce20ef06175
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-05-22 13:19:43 -0300

    Adicionando validação no save do PaymentService

--- Asaas/grails-app/services/asaas/PaymentService.groovy
+++ Asaas/grails-app/services/asaas/PaymentService.groovy
@@ -14,7 +14,12 @@ import asaas.adapter.PaymentSaveAdapter
 class PaymentService {
   
   public Payment save(PaymentSaveAdapter data){
-    
+    Payment validatedPayment = validateSave(data)
+
+    if (validatedPayment.hasErrors()){
+      throw new ValidationException("Error ao criar uma cobrança", validatedPayment.errors)
+    }
+
     Customer customer = Customer.load(data.customerId)
 
     Payer payer = Payer.load(data.payerId)
@@ -30,6 +35,39 @@ class PaymentService {
     payment.save(failOnError: true)
 
     return payment
+  }
+
+  private Payment validateSave(PaymentSaveAdapter data){
+    Payment payment = new Payment()
+
+    if (!data.value) {
+      payment.errors.reject("value", null, "Informe um valor válido")
+    }
+
+    if (!data.maturityDate) {
+      payment.errors.reject("maturityDate", null, "Informe uma data válida")
+    } else if (data.maturityDate < new Date()){
+      payment.errors.reject("maturityDate", null, "Informe uma data superior à atual")
+    }
+
+    if (!data.method) {
+      payment.errors.reject("method", null, "Informe uma forma de pagamento válida")
+    }
+
+    if (!data.status) {
+      payment.errors.reject("status", null, "Informe um status de pagamento válido")
+    }
+
+    if (!data.customerId) {
+      payment.errors.reject("customer", null, "Informe um cliente válido")
+    }
+
+    if (!data.payerId) {
+      payment.errors.reject("payer", null, "Informe um pagador válido")
+    }
+
+    return payment
+
   }
 
 }
