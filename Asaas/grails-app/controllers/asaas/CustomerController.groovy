b47dba5fd8591540359f880ef3079bc064234f53
anamilchert <ana.milchert@hotmail.com>
2024-05-22 14:14:37 -0300

    Criando edit e update

--- Asaas/grails-app/controllers/asaas/CustomerController.groovy
+++ Asaas/grails-app/controllers/asaas/CustomerController.groovy
@@ -4,33 +4,70 @@ import asaas.Customer
 import asaas.CustomerService
 import grails.validation.ValidationException
 
-class CustomerController {
+ class CustomerController {
 
-    def customerService
+        def customerService
 
-    def index() {
-        
-    }
-
-    def save() {
-        try {
-            Customer customer = customerService.save(params)
-            redirect(action: "show", id: customer.id)
-        } catch (ValidationException e) {
-            String errorsMessage = e.errors.allErrors.defaultMessage.join(", ")
-            flash.error = "Não foi possível salvar sua conta: $errorsMessage"
-            render(view: 'show', params: params)
+        def index() {
+            
         }
-    }
 
-    def show(Long id) {
-            Customer customer = Customer.read(id)
-                if (!customer) {
+        def save() {
+            try {
+                Customer customer = customerService.save(params)
+                redirect(action: "show", id: customer.id)
+                
+            } catch (ValidationException e) {
+                String errorsMessage = e.errors.allErrors.defaultMessage.join(", ")
+                flash.error = "Não foi possível salvar sua conta: $errorsMessage"
+                render(view: 'show', params: params)
+            }
+        }
+
+        def show(Long id) {
+                Customer customer = Customer.read(id)
+                    if (!customer) {
+                    flash.error = "Conta não encontrada"
+                    redirect(action: 'index')
+
+                    return 
+                }
+
+                render(view: 'show', model: [customer: customer])
+        }
+
+        def edit(Long id) {
+        Customer customer = Customer.get(id)
+        if (!customer) {
+            flash.error = "Conta não encontrada"
+            redirect(action: 'index')
+            return
+        }
+        
+        render(view: 'edit', model: [customer: customer])
+        }
+
+        def update(Long id) {
+            Customer customer = Customer.get(id)
+            if (!customer) {
                 flash.error = "Conta não encontrada"
                 redirect(action: 'index')
 
-                return 
+                return
             }
-            render(view: 'show', model: [customer: customer])
-    }
+
+            customer.properties = params
+
+            try {
+                customerService.save(customer)
+                flash.message = "Conta atualizada com sucesso"
+                redirect(action: "show", id: customer.id)
+
+            } catch (ValidationException e) {
+                String errorsMessage = e.errors.allErrors.collect { it.defaultMessage }.join(", ")
+                flash.error = "Não foi possível atualizar sua conta: $errorsMessage"
+
+                render(view: 'show', model: [customer: customer])
+            }
+        }   
 }
