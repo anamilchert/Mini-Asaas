e919c3fc5dd1ba3a085dcc5a21f811bbd8d476de
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-05-31 09:46:46 -0300

    mudando de PaymentSaveAdapter para PaymentAdapter

---
+++ Asaas/src/main/groovy/asaas/adapter/PaymentAdapter.groovy
@@ -0,0 +1,30 @@
+package asaas.adapter
+
+import asaas.utils.CurrencyUtils
+import asaas.utils.CustomDateUtils
+import asaas.PaymentStatus
+import asaas.PaymentType
+
+class PaymentAdapter {
+
+  Integer value
+
+  Date dueDate
+
+  PaymentType type
+
+  PaymentStatus status
+
+  Long customerId
+
+  Long payerId
+
+  public PaymentAdapter(Map params) {
+    this.value = CurrencyUtils.fromStringToInteger(params.value)
+    this.dueDate = CustomDateUtils.setTimeToEndOfDay(params.dueDate)
+    this.type = PaymentType.convert(params.type)
+    this.status = PaymentStatus.PENDING
+    this.customerId = params.customerId.toLong()
+    this.payerId = params.payerId.toLong()
+  }
+}
