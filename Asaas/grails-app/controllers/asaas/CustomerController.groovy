e2291951d2095091dc081ecfc3c8abf8d350607f
anamilchert <ana.milchert@hotmail.com>
2024-06-10 19:54:58 -0300

    Adicionando CustomerRepository no service e controller

--- Asaas/grails-app/controllers/asaas/CustomerController.groovy
+++ Asaas/grails-app/controllers/asaas/CustomerController.groovy
@@ -2,6 +2,7 @@ package asaas
 
 import asaas.Customer
 import asaas.CustomerService
+import asaas.repositories.CustomerRepository
 
 import grails.validation.ValidationException
 
@@ -24,8 +25,7 @@ import grails.validation.ValidationException
         }
 
         def show() {
-            Long id = params.id?.toLong()
-            Customer customer = Customer.read(id)
+            Customer customer = CustomerRepository.query([id: params.id.toLong()]).get()  as Customer
 
             if (!customer) {
                 flash.error = 'Conta não encontrada'
@@ -36,9 +36,7 @@ import grails.validation.ValidationException
         }
 
         def edit() {
-            Long id = params.id?.toLong()
-            Customer customer = Customer.get(id)
-
+            Customer customer = CustomerRepository.query([id: params.id.toLong()]).get()  as Customer
             if (!customer) {
                 flash.error = 'Conta não encontrada'
                 redirect(action: 'index')
@@ -50,14 +48,13 @@ import grails.validation.ValidationException
         def update() {
             Long id = params.id?.toLong()
                 try {
-                    Customer customer = customerService.update(id, params)
-                    flash.message = 'Conta atualizada com sucesso'
-                    redirect(action: 'show', id: customer.id)
-                    
-                    } catch (ValidationException e) {
-                    String errorsMessage = e.errors.allErrors.collect { it.defaultMessage }.join(', ')
-                    flash.error = "Não foi possível atualizar sua conta: $errorsMessage"
-                    render(view: 'edit', model: [customer: Customer.get(id)])
-                }
-        }
+                Customer customer = customerService.update(id, params)
+                flash.message = 'Conta atualizada com sucesso'
+                redirect(action: 'show', id: customer.id)
+                }  catch (ValidationException e) {
+                String errorsMessage = e.errors.allErrors.collect { it.defaultMessage }.join(', ')
+                flash.error = "Não foi possível atualizar sua conta: $errorsMessage"
+                render(view: 'edit', model: [customer: customerRepository.query([includeDeleted: false, id: id]).get() as Customer])
+            }
     }
+}
