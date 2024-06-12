331bad227e273c12c2a1b522ae039322ea2cc12d
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-06-12 16:06:50 -0300

    trocando nome do job, adicionando intervalo de execução e try catch

---
+++ Asaas/grails-app/jobs/asaas/ProcessOverduePaymentsJob.groovy
@@ -0,0 +1,25 @@
+package asaas
+
+import asaas.Payment
+import asaas.PaymentService
+
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
+
+class ProcessOverduePaymentsJob {
+
+    PaymentService paymentService
+
+    static triggers = {
+        cron name: "expiredPaymentTrigger",  cronExpression: "0 0/10 0-1 * * ?"
+    }
+
+    def processOverduePaymentsJob() {
+        try {
+            List<Payment> paymentList = paymentService.expiredPayment()
+        } catch (Exception exception) {
+            logger.error("Ocorreu um erro durante o processamento dos pagamentos vencidos: ${exception.message}", exception)
+        }
+    }
+}
