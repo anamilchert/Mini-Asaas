8ac2c62f26834d8555ba7d180a13ef8ed5aaea99
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-06-12 21:25:48 -0300

    configuração inicial do spring security

--- Asaas/grails-app/init/asaas/BootStrap.groovy
+++ Asaas/grails-app/init/asaas/BootStrap.groovy
@@ -1,9 +1,33 @@
 package asaas
 
+import asaas.User
+import asaas.Role
+import asaas.UserRole
+
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> addTestUser()
     }
     def destroy = {
+    }
+
+    @Transactional
+    void addTestUser() {
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+
+        def testUser = new User(username: 'me', password: 'password').save()
+
+        UserRole.create testUser, adminRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert User.count() == 1
+        assert Role.count() == 1
+        assert UserRole.count() == 1
     }
 }
