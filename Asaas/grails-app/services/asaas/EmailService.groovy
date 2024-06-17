90a6f8111c9456beb0f5808febc44d2114566689
anamilchert <ana.milchert@hotmail.com>
2024-06-17 11:35:59 -0300

    Melhorando a legibilidade do código

--- Asaas/grails-app/services/asaas/EmailService.groovy
+++ Asaas/grails-app/services/asaas/EmailService.groovy
@@ -10,51 +10,48 @@ class EmailService {
     def mailService
 
     public void sendCreatePaymentEmailToPayer(Payer payer, Payment payment) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy")
-        String formattedDueDate = dateFormat.format(payment.dueDate)
-
-        task {
-            mailService.sendMail {
-                to payer.email
-                subject "Criação de cobrança"
-                body "Olá ${payer.name},\nUma cobrança com valor de R\$${payment.value} foi criada e tem você como pagador.\nPor favor, se atente a data de validade: ${formattedDueDate}.\n\nAtenciosamente,\nMini Asaas."
-            }
-        }
+        sendEmail(payer.email, "Criação de cobrança", getCreatePaymentEmailBody(payer.name, payment))
     }
 
     public void sendStatusChangeEmailToPayer(Payer payer, Payment payment) {
+        sendEmail(payer.email, "Status da sua cobrança foi alterado", getStatusChangeEmailBody(payer.name, payment))
+    }
+
+    public void sendCreatePaymentEmailToCustomer(Customer customer, Payment payment) {
+        sendEmail(customer.email, "Criação de cobrança", getCreatePaymentEmailBody(customer.name, payment, payer))
+    }
+
+    public void sendStatusChangeEmailToCustomer(Customer customer, Payment payment) {
+        sendEmail(customer.email, "Status da sua cobrança foi alterado", getStatusChangeEmailBody(customer.name, payment))
+    }
+
+    private void sendEmail(String to, String subject, String body) {
         task {
             mailService.sendMail {
-                to payer.email
-                subject "Status da sua cobrança foi alterado"
-                body "Olá ${payer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nMini Asaas."
+                to to
+                subject subject
+                body body
             }
         }
     }
 
-    public void sendCreatePaymentEmailToCustomer(Payer payer, Payment payment) {
-        Customer customer = payer.customer
+    private String getCreatePaymentEmailBody(String name, Payment payment, Payer payer = null) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy")
         String formattedDueDate = dateFormat.format(payment.dueDate)
-
-        task {
-            mailService.sendMail {
-                to customer.email
-                subject "Criação de cobrança"
-                body "Olá ${customer.name},\nSua cobrança com valor de R\$${payment.value} foi criada e o pagador é ${payer.name}.\n\nAtenciosamente,\nMini Asaas."
-            }
-        }
+        String payerInfo = payer ? " e o pagador é ${payer.name}" : ""
+        return """Olá ${name},
+                    Uma cobrança com valor de R\$${payment.value} foi criada${payerInfo}.
+                    Por favor, se atente a data de validade: ${formattedDueDate}.
+                            
+                    Atenciosamente,
+                    Mini Asaas."""
     }
 
-    public void sendStatusChangeEmailToCustomer(Payer payer, Payment payment) {
-        Customer customer = payer.customer
-
-        task {
-            mailService.sendMail {
-                to customer.email
-                subject "Status da sua cobrança foi alterado"
-                body "Olá ${customer.name},\nO status da cobrança de número ${payment.id} foi alterado para: ${payment.paymentStatus}.\n\nAtenciosamente,\nMini Asaas."
-            }
-        }
+    private String getStatusChangeEmailBody(String name, Payment payment) {
+        return """Olá ${name},
+                    O status da cobrança de número ${payment.id} foi alterado para: ${payment.status}.
+                            
+                    Atenciosamente,
+                    Mini Asaas."""
     }
 }
