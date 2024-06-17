2cf8dac61072927c3baf092f2e25c033297c6a6b
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-06-17 11:48:53 -0300

    criando método para listar notificações não lidas e método para marcar ela como lida

--- Asaas/grails-app/services/asaas/NotificationService.groovy
+++ Asaas/grails-app/services/asaas/NotificationService.groovy
@@ -4,6 +4,7 @@ import asaas.Customer
 import asaas.Notification
 import asaas.NotificationType
 import asaas.NotificationStatus 
+import asaas.repositories.NotificationRepository
 import asaas.Payment
 
 import grails.gorm.transactions.Transactional
@@ -11,7 +12,7 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class NotificationService {
 
-    public create(NotificationType type, Long customerId, Long paymentId) {
+    public void create(NotificationType type, Long customerId, Long paymentId) {
         Notification notification = new Notification()
         notification.type = type
         notification.customer = Customer.load(customerId)
@@ -19,5 +20,18 @@ class NotificationService {
         notification.status = NotificationStatus.UNREAD
 
         notification.save(failOnError: true)
-    } 
+    }
+
+    public List<Notification> listUnreadNotifications(Long customerId) {
+        List<Notification> notificationUnreadList = NotificationRepository
+            .query([customerId: customerId, status: NotificationStatus.UNREAD]).list() as List<Notification>
+        println notificationUnreadList
+        return notificationUnreadList
+    }
+
+    public void markAsRead(Long notificationId, Long customerId) {
+        Notification notification = NotificationRepository.query([customerId: id, id: notificationId]).get() as Notification
+        notification.status = NotificationStatus.READ
+        notification.save(flush: true)
+    }
 }
